5f6de0d18500050cc11467ea35a215fd8890859e
Jonn Mostovoy <amarr.industrial@gmail.com>
2016-03-04 15:06:28 +0100

    Example of partial function.

--- src/Main.purs
+++ src/Main.purs
@@ -1,9 +1,26 @@
 module Main where
 
-import Prelude (Unit)
-import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, log)
+import Control.Monad.Eff            (Eff)
+import Control.Monad.Eff.Console    (CONSOLE, log)
+import Control.Monad.Eff.Exception  (EXCEPTION())
+import Data.Either                  (Either(..))
+import Data.Argonaut.Parser         (jsonParser)
+import Node.Encoding                (Encoding(UTF8))
+import Node.FS                      (FS())
+import Node.FS.Sync                 (readTextFile)
+import Partial.Unsafe               (unsafePartial)
+import Prelude
 
-main :: forall e. Eff (console :: CONSOLE | e) Unit
+
+type MainEff a = Eff ( console :: CONSOLE
+                     , fs      :: FS
+                     , err     :: EXCEPTION | a )
+
+main :: forall e. (MainEff e) Unit
 main = do
-  log "Hello sailor!"
+  x <- readTextFile UTF8 "priv/AllSets.json"
+  let y = (unsafePartial g <<< jsonParser) x
+  (log <<< show) y
+  where
+    g :: forall a b. (Partial) => Either a b -> b
+    g (Right x) = x
